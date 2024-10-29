5ea7cbb520dbdb9614897212e7fe77a64c6aa247
Sumith Sigtia <137398119+Sumithsigtia@users.noreply.github.com>
2024-10-29 10:51:54 +0530

    Update HotelDetails.tsx

--- src/app/output/demo/HotelDetails.tsx
+++ src/app/output/demo/HotelDetails.tsx
@@ -15,7 +15,7 @@ interface HotelData {
 
 const HotelDetails: React.FC<HotelDetailsProps> = ({ location, checkinDate, checkoutDate }) => {
   const [hotelData, setHotelData] = useState<HotelData[]>([]);
-  const baseUrl = 'https://reduce-soccer-whenever-hello.trycloudflare.com';
+  const baseUrl = 'https://cry-senior-news-trust.trycloudflare.com';
 
   const fetchHotelDetails = async () => {
     try {
