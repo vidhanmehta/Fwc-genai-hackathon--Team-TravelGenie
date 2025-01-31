fc3694f6abc18c903b3a47b0cd012b859a61bf9d
Vidhan Mehta <97885241+vidhanmehta@users.noreply.github.com>
2025-01-31 11:14:27 +0530

    Update FlightDetails.tsx

--- src/app/output/demo/FlightDetails.tsx
+++ src/app/output/demo/FlightDetails.tsx
@@ -15,7 +15,7 @@ interface FlightData {
 
 const FlightDetails: React.FC<FlightDetailsProps> = ({ source, destination, departureDate }) => {
   const [flightData, setFlightData] = useState<FlightData[]>([]);
-  const baseUrl = 'https://garcia-install-tracy-amanda.trycloudflare.com';
+  const baseUrl = 'https://tea-came-donations-councils.trycloudflare.com';
 
   const fetchFlightDetails = async () => {
     try {
